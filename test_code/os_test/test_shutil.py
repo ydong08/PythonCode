359857218782e962a0fb5b1a9d45a8bdf4805780
ydong08 <lfeng1108@163.com>
2018-04-27 14:46:17 +0800

    add test shutil file

---
+++ test_code/os_test/test_shutil.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+
+import os
+import shutil
+
+def main():
+    test_shutil()
+
+def test_shutil():
+    
+
+
+if __name__ == '__main__':
+    main()
