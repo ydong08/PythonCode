4f7548ecf852fc8ea33af3e4771355226dd802d9
ydong08 <lfeng1108@163.com>
2018-05-03 16:52:49 +0800

    update

--- test_code/network_test/test_network_cli.py
+++ test_code/network_test/test_network_cli.py
@@ -10,14 +10,9 @@ def client():
     print(host)
     port = 9999
     s.connect((host, port))
-
     msg = s.recv(1024)
-    
     s.close()
     print(msg.decode('utf-8'))
-
-
-
 
 def server():
     sersock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
