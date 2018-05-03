554629ca6a9085d3d3f0f6c0e82e746e38e9e38f
ydong08 <lfeng1108@163.com>
2018-05-03 09:12:07 +0800

    update network code

--- test_code/network_test/test_network.py
+++ test_code/network_test/test_network.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
+import sys
+import socket
+
 def client():
-    pass
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    host = socket.gethostname()
+    print(host)
+    port = 9999
+    s.connect((host, port))
+
+
 
 def server():
     pass
 
 def main():
-    pass
+    client()
 
 if __name__ == '__main__':
     main()
