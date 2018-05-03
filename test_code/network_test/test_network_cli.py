68c1e1cac9953763707f0aa1e5bbb588b08fbb4a
ydong08 <lfeng1108@163.com>
2018-05-03 09:25:38 +0800

    add files

---
+++ test_code/network_test/test_network_cli.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+import sys
+import socket
+
+def client():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    host = socket.gethostname()
+    print(host)
+    port = 9999
+    s.connect((host, port))
+
+    msg = s.recv(1024)
+    
+    s.close()
+    print(msg.decode('utf-8'))
+
+
+
+
+def server():
+    sersock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    host = socket.gethostname()
+    port = 9999
+    sersock.bind((host, port))
+    sersock.listen(10)
+
+    while True:
+        clisock, addr = sersock.accept()
+        print('connected addr: %s' % str(addr))
+        msg = 'welcome to caltels\r\n'
+        clisock.send(msg.encode('utf-8'))
+        clisock.close()
+
+def main():
+    client()
+
+if __name__ == '__main__':
+    main()
