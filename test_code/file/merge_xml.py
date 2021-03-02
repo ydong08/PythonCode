268f538608917565cdfc8eadbd04101606314c97
ydong08 <lfeng1108@163.com>
2021-03-02 11:20:54 +0800

    add  code

--- test_code/file/merge_xml.py
+++ test_code/file/merge_xml.py
@@ -1,6 +1,8 @@
-﻿import sys
-from xml.etree import ElementTree as et
+﻿# _*_ coding:utf-8 _*_
+#!/usr/bin/env python
 
+import sys
+from xml.etree import ElementTree as et
 
 class hashabledict(dict):
     def __hash__(self):
@@ -14,44 +16,45 @@ class XMLCombiner(object):
         self.roots = [et.parse(f).getroot() for f in filenames]
 
     def combine(self):
-      for r in self.roots[1:]:
+        for r in self.roots[1:]:
         # combine each element with the first one, and update that
-        self.combine_element(self.roots[0], r)
-    # return the string representation
-      return et.ElementTree(self.roots[0])
+            self.combine_element(self.roots[0], r)
+        # return the string representation
+        return et.ElementTree(self.roots[0])
 
     def combine_element(self, one, other):
-    """
-    This function recursively updates either the text or the children
-    of an element if another element is found in `one`, or adds it
-    from `other` if not found.
-    """
-    # Create a mapping from tag name to element, as that's what we are fltering with
-    mapping = {(el.tag, hashabledict(el.attrib)): el for el in one}
-    for el in other:
-        if len(el) == 0:
-            # Not nested
-            try:
-                # Update the text
-                mapping[(el.tag, hashabledict(el.attrib))].text = el.text
-            except KeyError:
-                # An element with this name is not in the mapping
-                mapping[(el.tag, hashabledict(el.attrib))] = el
-                # Add it
-                one.append(el)
-        else:
-            try:
-                # Recursively process the element, and update it in the same way
-                self.combine_element(mapping[(el.tag, hashabledict(el.attrib))], el)
-            except KeyError:
-                # Not in the mapping
-                mapping[(el.tag, hashabledict(el.attrib))] = el
-                # Just add it
-                one.append(el)
+        """
+        This function recursively updates either the text or the children
+        of an element if another element is found in `one`, or adds it
+        from `other` if not found.
+        """
+        # Create a mapping from tag name to element, as that's what we are fltering with
+        mapping = {(el.tag, hashabledict(el.attrib)): el for el in one}
+        for el in other:
+            if len(el) == 0:
+                # Not nested
+                try:
+                    # Update the text
+                    mapping[(el.tag, hashabledict(el.attrib))].text = el.text
+                except KeyError:
+                    # An element with this name is not in the mapping
+                    mapping[(el.tag, hashabledict(el.attrib))] = el
+                    # Add it
+                    one.append(el)
+            else:
+                try:
+                    # Recursively process the element, and update it in the same way
+                    self.combine_element(mapping[(el.tag, hashabledict(el.attrib))], el)
+                except KeyError:
+                    # Not in the mapping
+                    mapping[(el.tag, hashabledict(el.attrib))] = el
+                    # Just add it
+                    one.append(el)
 
 if __name__ == '__main__':
+    r = XMLCombiner(sys.argv[1:-1]).combine()
+    print '-'*20
+    print et.tostring(r.getroot())
+    r.write(sys.argv[-1], encoding="iso-8859-1", xml_declaration=True)
 
-  r = XMLCombiner(sys.argv[1:-1]).combine()
-  print '-'*20
-  print et.tostring(r.getroot())
-  r.write(sys.argv[-1], encoding="iso-8859-1", xml_declaration=True)
+
